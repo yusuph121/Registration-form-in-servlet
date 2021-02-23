0414d4c3b7262ab1302777d2bf14c258919a0a03
MOHAMMAD YUSUPH <77335532+yusuph121@users.noreply.github.com>
2021-02-23 11:16:14 +0530

    Add files via upload

---
+++ reg.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TABLE `reg` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reg`
+--
+
+INSERT INTO `reg` (`name`, `email`, `password`) VALUES
+('yusuph', 'moyu@gmail.com', '7675768978'),
+('', '', '');
+COMMIT;
+
